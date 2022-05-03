8beea1c0aa1e652d13efa70b13d9604c144e01ec
RobWi01 <rob.windey@gmail.com>
2022-05-03 16:50:13 +0200

    Fixed error

--- components/patientcard.tsx
+++ components/patientcard.tsx
@@ -42,7 +42,7 @@ export default function PatientCard({
     deletePatient(patient._id);
     fetch("/api/delete_patient", {
       method: "POST",
-      body: JSON.stringify({ new_patient: patient }),
+      body: JSON.stringify({ patient }),
       headers: {
         "Content-Type": "application/json",
       },
@@ -60,7 +60,7 @@ export default function PatientCard({
 
       fetch("/api/update_patient", {
         method: "POST",
-        body: JSON.stringify({ patient }),
+        body: JSON.stringify({ new_patient: patient }),
         headers: {
           "Content-Type": "application/json",
         },
