375de7e871176a4d08914c32c525782498cebc00
RobWi01 <rob.windey@gmail.com>
2022-05-06 12:36:13 +0200

    Changes Xander

--- components/PopUp.tsx
+++ components/PopUp.tsx
@@ -39,7 +39,7 @@ const PopUp = ({ file }: FileCardProps) => {
                 <button
                   onClick={() =>
                     window.open(
-                      "mailto:? &subject=Relu 3D image with annotations &body=Beste collega, \r\n Hierbij de link van patiënt : " +
+                      "mailto:? &subject=Relu 3D image with annotations &body=Beste collega, \r\n Hierbij de link van patient : " +
                         GetURL({ file })
                     )
                   }
@@ -56,7 +56,7 @@ const PopUp = ({ file }: FileCardProps) => {
             </div>
           </div>
 
-          <div className="h-10 flex items-center text-truncate overflow-auto">
+          <div className="h-10 flex items-center text-truncate hover:overflow-scroll">
             <div className="">{GetURL({ file })}</div>
           </div>
         </div>
